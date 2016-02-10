da0913e2df73cf62a4bc0af890705cc16b570d9b
David Gomez <dg.dave@gmail.com>
2016-02-10 00:41:34 -0500

    add Avatar and UserTooltip components to index

--- app/components/index.js
+++ app/components/index.js
@@ -1,1 +1,3 @@
 export { default as LoginForm } from './LoginForm/LoginForm';
+export { default as Avatar } from './Avatar/Avatar';
+export { default as UserTooltip } from './UserTooltip/UserTooltip';
