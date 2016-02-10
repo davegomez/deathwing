b76daa00991d45b1f6193fdde8257ea0da91566b
David Gomez <dg.dave@gmail.com>
2016-02-10 00:40:29 -0500

    update avatar component

--- app/components/Avatar/Avatar.js
+++ app/components/Avatar/Avatar.js
@@ -1,11 +1,12 @@
 import React, { PropTypes } from 'react';
-import { UserCard } from '../';
-import css from './_avatar.scss';
+import { UserTooltip } from '../';
+import css from './Avatar.scss';
 
 export default class Avatar extends React.Component {
   static propTypes = {
     userImageURL: PropTypes.string.isRequired,
-    userName: PropTypes.string.isRequired
+    userName: PropTypes.string.isRequired,
+    userEmail: PropTypes.string.isRequired
   };
 
   constructor(props) {
@@ -15,8 +16,12 @@ export default class Avatar extends React.Component {
   render() {
     return (
       <div className={css.avatar}>
-        <img src={this.props.userImageURL} alt={this.props.userName} />
-        <UserCard userData={this.props} />
+        <img
+          src={this.props.userImageURL}
+          alt={this.props.userName} />
+        <UserTooltip
+          userName={this.props.userName}
+          userEmail={this.props.userEmail} />
       </div>
     );
   }
