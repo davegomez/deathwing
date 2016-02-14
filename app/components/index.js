3daf961d9e820e5ba699aac5ce7325121e519c51
David Gomez <dg.dave@gmail.com>
2016-02-14 17:56:42 -0500

    add new components

--- app/components/index.js
+++ app/components/index.js
@@ -1,4 +1,6 @@
 export { default as LoginForm } from './LoginForm/LoginForm';
 export { default as Message } from './Message/Message';
+export { default as Icon } from './Icon/Icon';
+export { default as Counter } from './Counter/Counter';
 export { default as Avatar } from './Avatar/Avatar';
 export { default as UserTooltip } from './UserTooltip/UserTooltip';
