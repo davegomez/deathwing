6271f2a16831b7d68fd73e78758ee1e3194737b0
David Gomez <dg.dave@gmail.com>
2016-02-15 14:57:53 -0500

    add new components

--- app/components/index.js
+++ app/components/index.js
@@ -2,6 +2,7 @@ export { default as LoginForm } from './LoginForm/LoginForm';
 export { default as Message } from './Message/Message';
 export { default as MessageInput } from './MessageInput/MessageInput';
 export { default as MessageText } from './MessageText/MessageText';
+export { default as MessageMenu } from './MessageMenu/MessageMenu';
 export { default as Icon } from './Icon/Icon';
 export { default as Counter } from './Counter/Counter';
 export { default as Avatar } from './Avatar/Avatar';
