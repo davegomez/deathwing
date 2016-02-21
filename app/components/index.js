0e1c50872b0776c3e0d521d3cbb3bd1cab3807c7
David Gomez <dg.dave@gmail.com>
2016-02-21 14:35:27 -0500

    clean index.js from un-existence components

--- app/components/index.js
+++ app/components/index.js
@@ -1,12 +1,9 @@
 export { default as LoginForm } from './LoginForm/LoginForm';
 export { default as Message } from './Message/Message';
 export { default as MessageInput } from './MessageInput/MessageInput';
-export { default as MessageText } from './MessageText/MessageText';
 export { default as MessageMenu } from './MessageMenu/MessageMenu';
 export { default as Icon } from './Icon/Icon';
-export { default as Counter } from './Counter/Counter';
 export { default as Avatar } from './Avatar/Avatar';
-export { default as UserTooltip } from './UserTooltip/UserTooltip';
 export { default as Row } from './Row/Row';
 export { default as Column } from './Column/Column';
 export { default as Title } from './Title/Title';
