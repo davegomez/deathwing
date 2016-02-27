66837e87ab3da7dee28a07840825e17cdc364196
David Gomez <dg.dave@gmail.com>
2016-02-27 14:58:18 -0500

    add action to add and remove votes

--- app/actions/actionCreators.js
+++ app/actions/actionCreators.js
@@ -1,2 +1,8 @@
 export const SET_TOKEN_ID = 'SET_TOKEN_ID';
 export const setTokenId = tokenId => ({ type: SET_TOKEN_ID, tokenId });
+
+export const ADD_VOTE = 'ADD_VOTE';
+export const addVote = userId => ({ type: ADD_VOTE, userId });
+
+export const REMOVE_VOTE = 'REMOVE_VOTE';
+export const removeVote = userId => ({ type: REMOVE_VOTE, userId });
