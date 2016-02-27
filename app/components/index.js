ed88df16ad370a4b1f755b2285973228762df6a7
David Gomez <dg.dave@gmail.com>
2016-02-27 17:02:41 -0500

    update component index v2

--- app/components/index.js
+++ app/components/index.js
@@ -3,7 +3,6 @@ export { default as MessageMenu } from './MessageMenu/MessageMenu';
 export { default as LoginForm } from './LoginForm/LoginForm';
 export { default as Message } from './Message/Message';
 export { default as Avatar } from './Avatar/Avatar';
-export { default as Column } from './Column/Column';
 export { default as Intent } from './Intent/Intent';
 export { default as Icon } from './Icon/Icon';
 export { default as Row } from './Row/Row';
